e86004cf61c57a2f07821fce595760bc66a8b371
Jonah <jonahwert07@gmail.com>
2026-03-28 15:58:07 -0400

    Add Maven file

--- src/WebServiceReceive.java
+++ src/WebServiceReceive.java
@@ -1,2 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WebServiceReceive {
+
+    public static void main(String[] args) {
+        try {
+            // Specify the URL of the web service
+            String url = "http://localhost:8000/hello";
+
+            // Create a URL object
+            URL obj = new URL(url);
+
+            // Open a connection to the URL
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // Set the request method
+            con.setRequestMethod("GET");
+
+            // Set request headers if needed
+            // con.setRequestProperty("Content-Type", "application/json");
+
+            // Get the response code
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code : " + responseCode);
+
+            // Read the response from the web service
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Print the response
+            System.out.println("Response: " + response.toString());
+
+            // Parse the JSON response as needed
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
