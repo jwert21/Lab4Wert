a103087150b3884613212b8c9436691e06e83771
Jonah <jonahwert07@gmail.com>
2026-03-29 23:02:12 -0400

    Add Maven file

--- src/WebServiceReceive.java
+++ src/WebServiceReceive.java
@@ -19,7 +19,7 @@ public class WebServiceReceive {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello from Java HTTP Server";
+            String response = "Hello from Pizza Shop";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
