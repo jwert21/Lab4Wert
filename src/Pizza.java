bebb0b5ef8b1088aeb9784e883806b1b96d394c3
Jonah <jonahwert07@gmail.com>
2026-03-31 10:51:14 -0400

    Add Maven file

--- src/Pizza.java
+++ src/Pizza.java
@@ -9,32 +9,35 @@ public class Pizza {
         private String type;
         private String id;
         private String crust;
+        private String size;
 
-        public Pizza(String type, String id, String crust) {
+        public Pizza(String type, String id, String crust, String size) {
             this.type = type;
             this.id = id;
             this.crust = crust;
+            this.size = size;
         }
 
         public String toFixedFormatString() {
-            return String.format("%-10s%-10s%-20s", type, id, crust);
+            return String.format("%-10s%-10s%-20s", type, id, crust, size);
         }
 
         public static Pizza fromFixedFormatString(String line) {
             String type = line.substring(0, 10).trim();
             String id = line.substring(10, 20).trim();
             String crust = line.substring(20, 40).trim();
-            return new Pizza(type, id, crust);
+            String size = line.substring(40, 80).trim();
+            return new Pizza(type, id, crust, size);
         }
     }
 
     public static void main(String[] args) {
         List<Pizza> pizza = new ArrayList<>();
-        pizzas.add(new Pizza("Pepperoni", "1", "Neapolitan"));
-        pizzas.add(new Pizza("Cheese", "2", "New York-Style"));
-        pizzas.add(new Pizza("Sausage", "3", "Sicilian"));
-        pizzas.add(new Pizza("Vegetable", "4", "Focaccia"));
-        pizzas.add(new Pizza("Mushroom", "5", "New York-Style"));
+        pizzas.add(new Pizza("Pepperoni", "1", "Neapolitan", "Personal"));
+        pizzas.add(new Pizza("Cheese", "2", "New York-Style", "Small"));
+        pizzas.add(new Pizza("Sausage", "3", "Sicilian", "Medium"));
+        pizzas.add(new Pizza("Vegetable", "4", "Focaccia", "Large"));
+        pizzas.add(new Pizza("Mushroom", "5", "New York-Style", "Jumbo Large"));
 
         try (PrintWriter writer = new PrintWriter(new FileWriter("pizzas.txt"))) {
             for (Pizza pizza : pizzas) {
@@ -55,7 +58,7 @@ public class Pizza {
         }
 
         for (Pizza pizza : loadedPizzas) {
-            System.out.println("Name: " + pizza.type + ", ID: " + pizza.id + ", Crust: " + pizza.crust);
+            System.out.println("Type: " + pizza.type + ", ID: " + pizza.id + ", Crust: " + pizza.crust);
         }
     }
 }
