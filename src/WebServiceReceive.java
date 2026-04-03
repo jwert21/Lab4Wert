9b2411caa697ae6e8e6e696418871f770eafca5d
Jonah <jonahwert07@gmail.com>
2026-04-02 21:25:41 -0400

    Changes to RabbitMQReceive

--- src/WebServiceReceive.java
+++ src/WebServiceReceive.java
@@ -8,30 +8,62 @@
 
  */
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 
 public class WebServiceReceive {
+
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/hello", new MyHandler());
-        server.setExecutor(null); // creates a default executor
+        server.createContext("/pizzashop", new MyHandler());
+        server.setExecutor(null);
         server.start();
+
+        System.out.println("Server running on http://localhost:8000/pizzashop");
     }
 
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello from Pizza Shop";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+
+            if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
+
+                InputStream is = exchange.getRequestBody();
+                String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+                System.out.println("Received JSON: " + body);
+
+                Gson gson = new Gson();
+                Pizza pizza = gson.fromJson(body, Pizza.class);
+
+                System.out.println("Converted to Pizza object:");
+                System.out.println("Type: " + pizza.getType());
+                System.out.println("ID: " + pizza.getId());
+                System.out.println("Crust: " + pizza.getCrust());
+                System.out.println("Size: " + pizza.getSize());
+
+                String response = "Pizza received successfully!";
+                exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes(StandardCharsets.UTF_8));
+                os.close();
+
+            } else {
+                String response = "Only POST requests are supported.";
+                exchange.sendResponseHeaders(405, response.getBytes(StandardCharsets.UTF_8).length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes(StandardCharsets.UTF_8));
+                os.close();
+            }
         }
     }
 }
