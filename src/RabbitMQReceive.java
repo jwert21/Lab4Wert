cf6e60e0a280b864b202f0f0211d1f376b7cd56a
Jonah <jonahwert07@gmail.com>
2026-04-02 21:22:16 -0400

    Changes to RabbitMQReceive

--- src/RabbitMQReceive.java
+++ src/RabbitMQReceive.java
@@ -8,29 +8,51 @@
 
  */
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class RabbitMQReceive {
 
-    private final static String QUEUE_NAME = "Hello!";
+    private static final String QUEUE_NAME = "pizzashop_queue";
 
     public static void main(String[] argv) throws Exception {
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        System.out.println("Waiting for flat file messages...");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
-        };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+            String message = new String(delivery.getBody());
 
+            System.out.println("[*] Received flat file data:");
+            System.out.println(message);
+
+            // Convert back to Pizza object
+            Pizza newPizza = Pizza.fromFixedFormatString(message);
+
+            //Deserializing pizza object after receiving
+            System.out.println("\nConverted to Pizza object:");
+            System.out.println("Type: " + newPizza.getType());
+            System.out.println("ID: " + newPizza.getId());
+            System.out.println("Crust: " + newPizza.getCrust());
+            System.out.println("Size: " + newPizza.getSize());
+
+            //write received data to a file for comparison
+            try (PrintWriter writer = new PrintWriter(new FileWriter("pizzaFlatFile_received.txt"))) {
+                writer.println(message);
+            }
+
+            System.out.println("\nSaved received data to pizzaFlatFile_received.txt");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
 }
